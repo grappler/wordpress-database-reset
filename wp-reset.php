33f3192ab04ed1093b06e349c50ee8ae7e034c9c
Chris Berthe <chrisberthe@gmail.com>
2014-09-23 13:10:39 -0400

    Update to WordPress PHP coding standards

--- wp-reset.php
+++ wp-reset.php
@@ -10,7 +10,7 @@ License: GNU General Public License
 Text Domain: wp-reset
 */
 
-if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
+if ( ! class_exists( 'CB_WP_Reset' ) && is_admin() ) :
 
 	class CB_WP_Reset {
 
@@ -35,11 +35,11 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @return void
 		 */
 		function __construct() {
-			add_action('init', array($this, 'init_language'));
-			add_action('admin_init', array($this, 'wp_reset_init'));
-			add_action('admin_init', array($this, '_redirect_user'));
-			add_action('admin_menu', array($this, 'add_admin_menu'));
-			add_filter('wp_mail', array($this, '_fix_mail'));
+			add_action( 'init', array( $this, 'init_language' ) );
+			add_action( 'admin_init', array( $this, 'wp_reset_init' ) );
+			add_action( 'admin_init', array( $this, '_redirect_user' ) );
+			add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+			add_filter( 'wp_mail', array( $this, '_fix_mail' ) );
 		}
 
 		/**
@@ -55,36 +55,36 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 			$this->_wp_tables = $wpdb->tables();
 
 			// Check for valid input - goes ahead and drops / resets tables
-			if ( isset($_POST['wp-random-value'], $_POST['wp-reset-input']) && $_POST['wp-random-value'] == $_POST['wp-reset-input']
-				&& check_admin_referer('wp-nonce-submit', $this->_nonce) ) {
+			if ( isset( $_POST['wp-random-value'], $_POST['wp-reset-input']) && $_POST['wp-random-value'] == $_POST['wp-reset-input']
+				&& check_admin_referer( 'wp-nonce-submit', $this->_nonce ) ) {
 
 				require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
 
 				// No tables were selected
-				if ( ! isset($_POST['tables']) && empty($_POST['tables']) ) {
+				if ( ! isset( $_POST['tables']) && empty( $_POST['tables'] ) ) {
 					wp_redirect( admin_url( $pagenow ) . '?page=wp-reset&reset=no-select' ); exit();
 				}
 
 				// Get current options
-				$blog_title = get_option('blogname');
-				$public = get_option('blog_public');
+				$blog_title = get_option( 'blogname' );
+				$public = get_option( 'blog_public' );
 
-				$admin_user = get_user_by('login', 'admin');
-				$user = ( ! $admin_user || ! user_can($admin_user->ID, 'update_core') ) ? $current_user : $admin_user;
+				$admin_user = get_user_by( 'login', 'admin' );
+				$user = ( ! $admin_user || ! user_can( $admin_user->ID, 'update_core' ) ) ? $current_user : $admin_user;
 
 				// Get the selected tables
-				$tables = (isset($_POST['tables'])) ? array_flip($_POST['tables']) : array();
+				$tables = ( isset( $_POST['tables'] ) ) ? array_flip( $_POST['tables'] ) : array();
 
 				// Compare the selected tables against the ones in the database
-				$this->_tables = array_diff_key($this->_wp_tables, $tables);
+				$this->_tables = array_diff_key( $this->_wp_tables, $tables );
 
 				// Preserve the data from the tables that are unique
-				if ( 0 < count($this->_tables) ) {
-					$backup_tables = $this->_backup_tables($this->_tables);
+				if ( 0 < count( $this->_tables ) ) {
+					$backup_tables = $this->_backup_tables( $this->_tables );
 				}
 
 				// Grab the currently active plugins and theme
-				if ( isset($_POST['wp-reset-check']) && 'true' == $_POST['wp-reset-check'] ) {
+				if ( isset( $_POST['wp-reset-check']) && 'true' == $_POST['wp-reset-check'] ) {
 					$current_data['active-plugins'] = get_option( 'active_plugins' );
 					$current_data['current-theme'] = get_option( 'current_theme' );
 					$current_data['template'] = get_option( 'template' );
@@ -94,35 +94,37 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 				// Run through the database columns, drop all the tables and
 				// install wp with previous settings
 				if ( ! empty( $this->_wp_tables ) ) {
-					foreach ($this->_wp_tables as $wp_table) {
-						$wpdb->query("DROP TABLE {$wp_table}");
+					foreach ( $this->_wp_tables as $wp_table ) {
+						$wpdb->query( "DROP TABLE {$wp_table}" );
 					}
-					$keys = wp_install($blog_title, $user->user_login, $user->user_email, $public);
-					$this->_wp_update_user($user, $keys);
+					$keys = wp_install( $blog_title, $user->user_login, $user->user_email, $public );
+					$this->_wp_update_user( $user, $keys );
 				}
 
 				// Delete and replace tables with the backed up table data
-				if ( ! empty($backup_tables) ) {
-					foreach ($this->_tables as $table) {
-						$wpdb->query("DELETE FROM " . $table);
+				if ( ! empty( $backup_tables ) ) {
+					foreach ( $this->_tables as $table ) {
+						$wpdb->query( "DELETE FROM " . $table );
 					}
-					$this->_backup_tables($backup_tables, 'reset');
+					$this->_backup_tables( $backup_tables, 'reset' );
 				}
 
 				if ( ! empty( $current_data ) ) {
 					update_option( 'active_plugins', $current_data['active-plugins'] );
 
-					if ( ! empty( $current_data['current-theme']) ) {
+					if ( ! empty( $current_data['current-theme'] ) ) {
 						update_option( 'current_theme', $current_data['current-theme'] );
 					}
 
 					update_option( 'template', $current_data['template'] );
 					update_option( 'stylesheet', $current_data['stylesheet'] );
 
-					wp_redirect( admin_url($pagenow) . '?page=wp-reset&reset=success' ); exit();
+					wp_redirect( admin_url($pagenow) . '?page=wp-reset&reset=success' );
+					exit();
 				}
 
-				wp_redirect( admin_url() ); exit();
+				wp_redirect( admin_url() );
+				exit();
 			}
 		}
 
@@ -136,53 +138,53 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 			global $current_user;
 
 			// Return to see if admin object exists
-			$admin_user = get_user_by('login', 'admin');
-			$random_string = wp_generate_password(5, false); ?>
+			$admin_user = get_user_by( 'login', 'admin' );
+			$random_string = wp_generate_password( 5, false ); ?>
 
-			<?php if ( isset($_POST['wp-random-value'], $_POST['wp-reset-input']) && $_POST['wp-random-value'] != $_POST['wp-reset-input'] ) : ?>
-				<div class="error"><p><strong><?php _e('You entered the wrong value - please try again', 'wp-reset') ?>.</strong></p></div>
-			<?php elseif ( isset($_GET['reset']) && 'no-select' == $_GET['reset'] ) : ?>
-				<div class="error"><p><strong><?php _e('You did not select any database tables', 'wp-reset') ?>.</strong></p></div>
-			<?php elseif ( isset($_GET['reset']) && 'success' == $_GET['reset'] ) : ?>
-				<div class="updated"><p><strong><?php _e('The WordPress database has been reset successfully', 'wp-reset') ?>.</strong></p></div>
+			<?php if ( isset( $_POST['wp-random-value'], $_POST['wp-reset-input'] ) && $_POST['wp-random-value'] != $_POST['wp-reset-input'] ) : ?>
+				<div class="error"><p><strong><?php _e( 'You entered the wrong value - please try again', 'wp-reset' ) ?>.</strong></p></div>
+			<?php elseif ( isset( $_GET['reset'] ) && 'no-select' == $_GET['reset'] ) : ?>
+				<div class="error"><p><strong><?php _e( 'You did not select any database tables', 'wp-reset' ) ?>.</strong></p></div>
+			<?php elseif ( isset( $_GET['reset'] ) && 'success' == $_GET['reset'] ) : ?>
+				<div class="updated"><p><strong><?php _e( 'The WordPress database has been reset successfully', 'wp-reset' ) ?>.</strong></p></div>
 			<?php endif ?>
 
 			<div class="wrap">
 				<?php screen_icon() ?>
-				<h2><?php _e('Database Reset', 'wp-reset') ?></h2>
+				<h2><?php _e( 'Database Reset', 'wp-reset' ) ?></h2>
 				<form action="" method="POST" id="wp-reset-form">
-					<p><?php _e('Please choose from the following database tables the ones you would like to reset', 'wp-reset') ?>:</p>
+					<p><?php _e( 'Please choose from the following database tables the ones you would like to reset', 'wp-reset' ) ?>:</p>
 					<div id="select-buttons">
-						<span><a href='#' id="select-all"><?php _e('Select All', 'wp-reset') ?></a></span>
+						<span><a href='#' id="select-all"><?php _e( 'Select All', 'wp-reset' ) ?></a></span>
 						<select id="wp-tables" multiple="multiple" name="tables[]">
 							<?php foreach ( $this->_wp_tables as $key => $value ) : ?>
 								<option value="<?php echo $key ?>"><?php echo $key ?></option>
 							<?php endforeach ?>
 						</select>
 					</div>
-					<p><?php _e('Type in (or copy/paste) the generated value into the text box', 'wp-reset') ?>:&nbsp;&nbsp;<strong><?php echo $random_string ?></strong></p>
-					<?php wp_nonce_field('wp-nonce-submit', $this->_nonce) ?>
+					<p><?php _e( 'Type in (or copy/paste) the generated value into the text box', 'wp-reset' ) ?>:&nbsp;&nbsp;<strong><?php echo $random_string ?></strong></p>
+					<?php wp_nonce_field( 'wp-nonce-submit', $this->_nonce ) ?>
 					<input type="hidden" name="wp-random-value" value="<?php echo $random_string ?>" id="wp-random-value" />
 					<input type="text" name="wp-reset-input" value="" id="wp-reset-input" />
-					<input type="submit" name="wp-reset-submit" value="<?php _e('Reset Database', 'wp-reset') ?>" id="wp-reset-submit" class="button-primary" />
-					<img src="<?php echo plugins_url('css/i/ajax-loader.gif', __FILE__) ?>" alt="loader" id="loader" style="display: none" />
+					<input type="submit" name="wp-reset-submit" value="<?php _e( 'Reset Database', 'wp-reset' ) ?>" id="wp-reset-submit" class="button-primary" />
+					<img src="<?php echo plugins_url( 'css/i/ajax-loader.gif', __FILE__ ) ?>" alt="loader" id="loader" style="display: none" />
 					<div id="reactivate" style="display: none">
 						<p>
 							<label for="wp-reset-check">
 								<input type="checkbox" name="wp-reset-check" id="wp-reset-check" checked="checked" value="true" />
-							<?php _e('Reactivate current plugins and theme after reset?', 'wp-reset') ?>
+							<?php _e( 'Reactivate current plugins and theme after reset?', 'wp-reset' ) ?>
 							</label>
 						</p>
 					</div>
 				</form>
 
-				<?php if ( ! $admin_user || ! user_can($admin_user->ID, 'update_core') ) : ?>
-					<p style="margin-top: 25px"><?php printf( __('The default user <strong><u>admin</u></strong> was never created for this WordPress install. So <strong><u>%s</u></strong> will be recreated with its current password instead', 'wp-reset'), $current_user->user_login ) ?>.</p>
+				<?php if ( ! $admin_user || ! user_can( $admin_user->ID, 'update_core' ) ) : ?>
+					<p style="margin-top: 25px"><?php printf( __( 'The default user <strong><u>admin</u></strong> was never created for this WordPress install. So <strong><u>%s</u></strong> will be recreated with its current password instead', 'wp-reset' ), $current_user->user_login ) ?>.</p>
 				<?php else : ?>
-					<p><?php _e('The default user <strong><u>admin</u></strong> will be recreated with its current password upon resetting', 'wp-reset') ?>.</p>
+					<p><?php _e( 'The default user <strong><u>admin</u></strong> will be recreated with its current password upon resetting', 'wp-reset' ) ?>.</p>
 				<?php endif ?>
 
-				<p><?php _e('Note that once you reset the database, all users will be deleted except the initial admin user.', 'wp-reset') ?></p>
+				<p><?php _e( 'Note that once you reset the database, all users will be deleted except the initial admin user.', 'wp-reset' ) ?></p>
 			</div>
 <?php	}
 
@@ -210,7 +212,7 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 					});
 
 					$('#wp-reset-submit').click(function() {
-						var message = "<?php _e('Clicking OK will result in your database being reset to its initial settings. Continue?', 'wp-reset') ?>";
+						var message = "<?php _e( 'Clicking OK will result in your database being reset to its initial settings. Continue?', 'wp-reset' ) ?>";
 						var reset = confirm(message);
 
 						if (reset) {
@@ -238,9 +240,9 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @return void
 		 */
 		function add_admin_menu() {
-			if ( current_user_can('update_core') ) {
-				$this->_hook = add_submenu_page('tools.php', 'Database Reset', 'Database Reset', 'update_core', 'wp-reset', array($this, 'show_admin_page'));
-				add_action('load-' . $this->_hook, array($this, 'option_page_actions'));
+			if ( current_user_can( 'update_core' ) ) {
+				$this->_hook = add_submenu_page( 'tools.php', 'Database Reset', 'Database Reset', 'update_core', 'wp-reset', array( $this, 'show_admin_page' ) );
+				add_action( 'load-' . $this->_hook, array( $this, 'option_page_actions' ) );
 			}
 		}
 
@@ -250,8 +252,8 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @return void
 		 */
 		function option_page_actions() {
-			add_action('admin_enqueue_scripts', array($this, 'add_plugin_styles_and_scripts'));
-			add_action('admin_footer', array($this, 'add_admin_javascript'));
+			add_action( 'admin_enqueue_scripts', array( $this, 'add_plugin_styles_and_scripts' ) );
+			add_action( 'admin_footer', array( $this, 'add_admin_javascript' ) );
 			$this->_add_help_screen();
 		}
 
@@ -292,11 +294,11 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @return void
 		 */
 		function add_plugin_styles_and_scripts() {
-			wp_enqueue_style('wordpress-reset-css', plugins_url('css/wp-reset.css', __FILE__));
-			wp_enqueue_style('bsmselect-css', plugins_url('css/jquery.bsmselect.css', __FILE__));
+			wp_enqueue_style( 'wordpress-reset-css', plugins_url( 'css/wp-reset.css', __FILE__ ) );
+			wp_enqueue_style( 'bsmselect-css', plugins_url( 'css/jquery.bsmselect.css', __FILE__ ) );
 
-			wp_enqueue_script('bsmselect', plugins_url('js/jquery.bsmselect.js', __FILE__));
-			wp_enqueue_script('bsmselect-compatibility', plugins_url('js/jquery.bsmselect.compatibility.js', __FILE__));
+			wp_enqueue_script( 'bsmselect', plugins_url( 'js/jquery.bsmselect.js', __FILE__ ) );
+			wp_enqueue_script( 'bsmselect-compatibility', plugins_url( 'js/jquery.bsmselect.compatibility.js', __FILE__ ) );
 		}
 
 		/**
@@ -306,8 +308,8 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @return void
 		 */
 		function init_language() {
-			$language_dir = basename(dirname(__FILE__)) . '/languages';
-			load_plugin_textdomain('wp-reset', false, $language_dir);
+			$language_dir = basename( dirname( __FILE__ ) ) . '/languages';
+			load_plugin_textdomain( 'wp-reset', false, $language_dir );
 		}
 
 		/**
@@ -317,7 +319,7 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @return void
 		 */
 		function plugin_activate() {
-			add_option('wp-reset-activated', true);
+			add_option( 'wp-reset-activated', true );
 		}
 
 		/**
@@ -327,9 +329,9 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @return void
 		 */
 		function _redirect_user() {
-			if ( get_option('wp-reset-activated', false) ) {
-				delete_option('wp-reset-activated');
-				wp_redirect(admin_url('tools.php') . '?page=wp-reset');
+			if ( get_option( 'wp-reset-activated', false ) ) {
+				delete_option( 'wp-reset-activated' );
+				wp_redirect( admin_url( 'tools.php' ) . '?page=wp-reset' );
 			}
 		}
 
@@ -342,14 +344,14 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @return $mail Version with password changed
 		 */
 		function _fix_mail($mail) {
-			$subject = __('WordPress Database Reset', 'wp-reset');
-			$message = __('The tables you selected have been successfully reset to their default settings:', 'wp-reset');
-			$password = __('Password: The password you chose during the install.', 'wp-reset');
+			$subject = __( 'WordPress Database Reset', 'wp-reset' );
+			$message = __( 'The tables you selected have been successfully reset to their default settings:', 'wp-reset' );
+			$password = __( 'Password: The password you chose during the install.', 'wp-reset' );
 
-			if ( stristr($mail['message'], 'Your new WordPress site has been successfully set up at:') ) {
-				$mail['subject'] = preg_replace('/New WordPress Site/', $subject, $mail['subject']);
-				$mail['message'] = preg_replace('/Your new WordPress site has been successfully set up at:+/', $message, $mail['message']);
-				$mail['message'] = preg_replace('/Password:\s.+/', $password, $mail['message']);
+			if ( stristr( $mail['message'], 'Your new WordPress site has been successfully set up at:' ) ) {
+				$mail['subject'] = preg_replace( '/New WordPress Site/', $subject, $mail['subject'] );
+				$mail['message'] = preg_replace( '/Your new WordPress site has been successfully set up at:+/', $message, $mail['message'] );
+				$mail['message'] = preg_replace( '/Password:\s.+/', $password, $mail['message'] );
 			}
 			return $mail;
 		}
@@ -362,27 +364,27 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @access private
 		 * @return array Backed up data if type backup, void if reset
 		 */
-		function _backup_tables($tables, $type = 'backup') {
+		function _backup_tables( $tables, $type = 'backup' ) {
 			global $wpdb;
 
-			if ( is_array($tables) ) {
+			if ( is_array( $tables ) ) {
 				switch ( $type ) {
 					case 'backup':
 						$backup_tables = array();
 						foreach ( $tables as $table ) {
-							$backup_tables[$table] = $wpdb->get_results("SELECT * FROM " . $table);
+							$backup_tables[$table] = $wpdb->get_results( "SELECT * FROM " . $table );
 						}
 						return $backup_tables;
 						break;
 					case 'reset':
 						foreach ( $tables as $table_name => $table_data ) {
-							foreach ($table_data as $row) {
+							foreach ( $table_data as $row ) {
 								$columns = $values = array();
 								foreach ( $row as $column => $value ) {
 									$columns[] = $column;
-									$values[] = esc_sql($value);
+									$values[] = esc_sql( $value );
 								}
-								$wpdb->query("INSERT INTO $table_name (" . implode(', ', $columns) . ") VALUES ('" . implode("', '", $values) . "')");
+								$wpdb->query( "INSERT INTO $table_name (" . implode( ', ', $columns ) . ") VALUES ('" . implode( "', '", $values ) . "')" );
 							}
 						}
 						break;
@@ -400,18 +402,20 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 		 * @param $keys Array returned by wp_install()
 		 * @return true on install success, false otherwise
 		 */
-		function _wp_update_user($user, $keys) {
+		function _wp_update_user( $user, $keys ) {
 			global $wpdb;
-			extract($keys, EXTR_SKIP);
+			extract( $keys, EXTR_SKIP );
 
-			$query = $wpdb->prepare("UPDATE $wpdb->users SET user_pass = '%s', user_activation_key = '' WHERE ID = '%d'", $user->user_pass, $user_id);
+			$query = $wpdb->prepare( "UPDATE $wpdb->users SET user_pass = '%s', user_activation_key = '' WHERE ID = '%d'", $user->user_pass, $user_id );
 
-			if ( $wpdb->query($query) ) {
+			if ( $wpdb->query( $query ) ) {
 				// Remove password reminder after installing
-				if ( get_user_meta($user_id, 'default_password_nag') ) delete_user_meta($user_id, 'default_password_nag');
+				if ( get_user_meta( $user_id, 'default_password_nag' ) ) {
+					delete_user_meta( $user_id, 'default_password_nag' );
+				}
 
 				wp_clear_auth_cookie();
-				wp_set_auth_cookie($user_id);
+				wp_set_auth_cookie( $user_id );
 
 				return true;
 			}
@@ -420,6 +424,6 @@ if ( ! class_exists('CB_WP_Reset') && is_admin() ) :
 	}
 
 	$cb_wp_reset = new CB_WP_Reset();
-	register_activation_hook( __FILE__, array('cb_wp_reset', 'plugin_activate') );
+	register_activation_hook( __FILE__, array( 'cb_wp_reset', 'plugin_activate' ) );
 
 endif;
