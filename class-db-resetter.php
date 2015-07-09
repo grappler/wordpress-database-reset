841f56a5a0e491a9165a13fe721f48d680e53101
Chris Berthe <chrisberthe@gmail.com>
2015-07-09 00:20:33 -0400

    Update indentation

--- class-db-resetter.php
+++ class-db-resetter.php
@@ -101,10 +101,12 @@ if ( ! class_exists( 'DB_Resetter' ) ) :
     private function update_user_settings() {
       global $wpdb;
 
-      $query = $wpdb->prepare( "UPDATE $wpdb->users
-                                SET user_pass = '%s', user_activation_key = ''
-                                WHERE ID = '%d'",
-                                $this->user->user_pass, $this->user->ID );
+      $query = $wpdb->prepare(
+               "UPDATE $wpdb->users
+                SET user_pass = '%s', user_activation_key = ''
+                WHERE ID = '%d'",
+                $this->user->user_pass, $this->user->ID
+              );
 
       $wpdb->query( $query );
       $this->remove_password_nag( $this->user->ID );
