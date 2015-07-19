417d1ad7a349cbc70d11fe938501ec21cf186d02
Chris Berthe <chrisberthe@gmail.com>
2015-07-18 23:29:07 -0400

    Change user-disclaimer to disclaimer

--- views/partials/disclaimer.php
+++ views/partials/disclaimer.php
@@ -1,3 +1,3 @@
-<p id="user-disclaimer" style="display: none">
+<p id="disclaimer" style="display: none">
   <em><?php printf( __( 'You selected the users table. Only the <strong><u>%s</u></strong> user will be restored', 'wp-reset' ), $this->user->user_login ) ?>.</em>
 </p>
