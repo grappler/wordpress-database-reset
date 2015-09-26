a10b4c4ea63556bbe64513f7918c5408f1072f14
Chris Berthe <chrisberthe@gmail.com>
2015-09-26 08:23:10 -0400

    Fix 'Plugin disabled after update' #17

--- wp-reset.php
+++ wp-reset.php
@@ -3,14 +3,14 @@
 Plugin Name: WordPress Database Reset
 Plugin URI: https://github.com/chrisberthe/wordpress-database-reset
 Description: A plugin that allows you to skip the 5 minute installation and reset WordPress's database back to its original state.
-Version: 3.0
+Version: 3.0.1
 Author: Chris Berthe
 Author URI: https://github.com/chrisberthe
 License: GNU General Public License
 Text-domain: wp-reset
 */
 
-define( 'DB_RESET_VERSION', '3.0' );
+define( 'DB_RESET_VERSION', '3.0.1' );
 define( 'DB_RESET_PATH', dirname( __FILE__ ) );
 define( 'DB_RESET_NAME', basename( DB_RESET_PATH ) );
 define( 'AUTOLOADER', DB_RESET_PATH . '/lib/class-plugin-autoloader.php' );
